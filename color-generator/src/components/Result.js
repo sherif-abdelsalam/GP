386c47ce38228e4ff7db11a120703ddb138aa033
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-07-13 02:05:48 +0300

    handle paletts

--- color-generator/src/components/Result.js
+++ color-generator/src/components/Result.js
@@ -1,29 +1,29 @@
 import "./Result.css";
 
 function Result({ colorPalette }) {
-    return (
-        <div id="result">
-            <div className="box">
-                {Array(6) // Create 6 boxes dynamically
-                    .fill(null)
-                    .map((_, index) => (
-                        <div key={index}>
-                            <div
-                                className="color-box"
-                                style={{
-                                    backgroundColor: colorPalette
-                                        ? colorPalette[index]
-                                        : "transparent", // Apply color or transparent
-                                }}
-                            ></div>
-                            <div className="color-code">
-                                {colorPalette ? colorPalette[index] : ""}
-                            </div>
-                        </div>
-                    ))}
+  return (
+    <div id="result">
+      <div className="box">
+        {Array(4) // Create 6 boxes dynamically
+          .fill(null)
+          .map((_, index) => (
+            <div key={index}>
+              <div
+                className="color-box"
+                style={{
+                  backgroundColor: colorPalette
+                    ? colorPalette[index]
+                    : "transparent", // Apply color or transparent
+                }}
+              ></div>
+              <div className="color-code">
+                {colorPalette ? colorPalette[index] : ""}
+              </div>
             </div>
-        </div>
-    );
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Result;
